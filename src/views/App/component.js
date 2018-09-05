52e34392a47d5dec2aed645418919550bff0580a
Piyush Jain <34448956+jainpiyush123@users.noreply.github.com>
2018-09-05 23:10:12 +0530

    freq chages

--- src/views/App/component.js
+++ src/views/App/component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from "prop-types"
 import {
   Router,
@@ -54,7 +54,7 @@ const StyledApp = styled.div`
   // Styles go here
 `
 
-class App extends Component {
+class App extends PureComponent {
   static contextTypes = {
     router: PropTypes.object
   }
